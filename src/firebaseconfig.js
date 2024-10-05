7586e0614195426e02458cf62647dbac52c8171f
Nicolas <ncorantehuenchuman@gmail.com>
2024-10-04 22:32:14 -0300

    Establecer conexion con firebase y realizar deploy de la pagina

---
+++ src/firebaseconfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCA7yu6duA2uva0Y-xx3GG1knIJpFEZe54',
+  authDomain: 'prueba-vue-8705c.firebaseapp.com',
+  projectId: 'prueba-vue-8705c',
+  storageBucket: 'prueba-vue-8705c.appspot.com',
+  messagingSenderId: '669873294671',
+  appId: '1:669873294671:web:d2f67323a853bb7813093a'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export default app
